78a0d38f558832beeafff8b90841674760ff77e3
Misan94 <misan@intense.ng>
2025-11-10 15:17:53 +0100

    feat: add Google Tag Manager tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,8 +86,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html
+      lang="en"
+      suppressHydrationWarning={true}
+    >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WKTV2K2D');`,
+          }}
+        />
         {/* Structured Data */}
         <script
           type="application/ld+json"
@@ -110,6 +123,15 @@ export default function RootLayout({
         />
       </head>
       <body className={`${lato.variable} antialiased font-lato`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WKTV2K2D"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <GoogleAnalytics measurementId={GA_MEASUREMENT_ID} />
         <CountryProvider>{children}</CountryProvider>
         <Analytics />
