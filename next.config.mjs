1d629b2278a442f77c727e1be61ede3dcfcde271
Dakins <akinbamizacheus@gmail.com>
2026-07-18 16:36:52 +0100

    update events link

--- next.config.mjs
+++ next.config.mjs
@@ -91,7 +91,7 @@ const nextConfig = {
       { source: '/join', destination: '/#waitlist', permanent: true },
       { source: '/creators', destination: '/#waitlist', permanent: true },
       { source: '/creator-community', destination: '/#waitlist', permanent: true },
-      { source: '/event', destination: 'https://zoom.us/meeting/register/c8A-ocLGSgCy3Ww90OvrqA', permanent: false },
+      { source: '/event', destination: 'https://zoom.us/j/96022115801?pwd=SoZpkacKX7gujAO5lYYRWgx5Oxw21W.1', permanent: false },
     ];
   },
 
